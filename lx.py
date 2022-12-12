59265fdacfe5cd2d35ca6837a9ff42c3a001efb8
ariful2000 <57025439+ariful2000@users.noreply.github.com>
2022-12-11 19:22:24 -0500

    Update lx.py

--- lx.py
+++ lx.py
@@ -1,6 +1,6 @@
 from sly import Lexer
 
-class BasicLexer(Lexer):
+class Lex(Lexer):
     
     literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';' }
 
@@ -46,11 +46,11 @@ class BasicLexer(Lexer):
 
 
 if __name__ == '__main__':
-    lexer = BasicLexer()
+    lexer = Lex()
     env = {}
     while True:
         try:
-            text = input('basic > ')
+            text = input('Start ==> ')
         except EOFError:
             break
         if text:
